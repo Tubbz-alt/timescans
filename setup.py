486d97cf107403e403a79b2ccb3268b7a9103681
TJ Lane <tjlane@stanford.edu>
2016-01-18 14:40:00 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+#coding: utf8
+
+"""
+Setup script for timescans.
+"""
+
+from glob import glob
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='timescans',
+      version='0.0.1',
+      author="TJ Lane",
+      author_email="tjlane@slac.stanford.edu",
+      description='time scan control',
+      packages=["timescans"],
+      package_dir={"timescans": "trapdoor"},
+      scripts=[s for s in glob('scripts/*') if not s.endswith('__.py')],
+      test_suite="test")
